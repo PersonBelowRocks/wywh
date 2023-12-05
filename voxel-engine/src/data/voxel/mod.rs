096439424098fe4947d8bb5c41600a24c8f28caf
ulrcon01 <ulrik.with.conradsen@tromso.kommune.no>
2023-12-05 14:22:26 +0100

    rotatable block models

--- voxel-engine/src/data/voxel/mod.rs
+++ voxel-engine/src/data/voxel/mod.rs
@@ -2,6 +2,8 @@ use bevy::math::Vec2;
 
 use crate::util::FaceMap;
 
+use self::rotations::{BlockModelFace, BlockModelFaceMap, BlockModelRotation};
+
 use super::{
     texture::FaceTexture,
     tile::{Face, Transparency},
@@ -17,18 +19,29 @@ pub struct VoxelProperties {
 
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
 pub struct BlockModel {
-    pub textures: FaceMap<FaceTexture>,
+    pub textures: BlockModelFaceMap<FaceTexture>,
 }
 
 impl BlockModel {
     pub fn filled(tex_pos: Vec2) -> Self {
         Self {
-            textures: FaceMap::filled(FaceTexture::new(tex_pos)),
+            textures: BlockModelFaceMap::filled(FaceTexture::new(tex_pos)),
         }
     }
 
-    pub fn texture(&self, face: Face) -> FaceTexture {
-        *self.textures.get(face).unwrap()
+    pub fn faces_for_rotation(&self, rotation: BlockModelRotation) -> FaceMap<FaceTexture> {
+        let mut map = FaceMap::new();
+        for face in BlockModelFace::FACES {
+            if let Some(tex) = self.texture(face) {
+                map.set(rotation.get_cardinal_face(face), tex);
+            }
+        }
+
+        map
+    }
+
+    pub fn texture(&self, face: BlockModelFace) -> Option<FaceTexture> {
+        self.textures.get(face).copied()
     }
 }
 
