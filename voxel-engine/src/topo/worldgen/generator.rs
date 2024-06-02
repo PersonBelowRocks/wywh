746385396592d627221f89e9881d895157202097
PersonBelowRocks <ulrikwithc@gmail.com>
2024-06-02 13:44:52 +0200

    massively speed up the generator at the cost of some quality and smoothness by doing some hueristic checks and math

--- voxel-engine/src/topo/worldgen/generator.rs
+++ voxel-engine/src/topo/worldgen/generator.rs
@@ -12,7 +12,7 @@ use crate::{
         voxel::rotations::BlockModelRotation,
     },
     topo::{
-        block::{BlockVoxel, Microblock},
+        block::{BlockVoxel, Microblock, SubdividedBlock},
         error::ChunkAccessError,
         world::{chunk_ref::ChunkRefAccess, Chunk, ChunkAccessInput, ChunkPos},
         MbWriteBehaviour, SubdivAccess,
@@ -71,6 +71,30 @@ impl Generator {
         }
     }
 
+    pub fn noise(&self, pos: IVec3) -> f64 {
+        self.noise.get((pos.as_dvec3() * self.scale).to_array())
+    }
+
+    /// Calculate the average noise from all 8 microblock corners of the given position.
+    pub fn block_corner_noise(&self, pos: IVec3) -> f64 {
+        let mut total = 0.0;
+
+        for x in [0, 3] {
+            for y in [0, 3] {
+                for z in [0, 3] {
+                    total += self.noise_mb((pos * SubdividedBlock::SUBDIVISIONS) + ivec3(x, y, z));
+                }
+            }
+        }
+
+        total / 8.0
+    }
+
+    pub fn noise_mb(&self, pos_mb: IVec3) -> f64 {
+        self.noise
+            .get((pos_mb.as_dvec3() * (self.scale / 4.0)).to_array())
+    }
+
     #[inline]
     pub fn write_to_chunk<'chunk>(
         &self,
@@ -90,6 +114,8 @@ impl Generator {
             MbWriteBehaviour::Ignore,
             Microblock::new(self.palette.void),
         );
+
+        let stone_block = BlockVoxel::new_full(self.palette.stone);
 
         let ws_min = cs_pos.worldspace_min();
         let ws_min_sd = ws_min * 4;
@@ -115,23 +141,38 @@ impl Generator {
             ChunkAccessInput::new(BlockVoxel::new_full(self.palette.debug)),
         )?;
 
-        for sd_x in 0..Chunk::SUBDIVIDED_CHUNK_SIZE {
-            for sd_y in 0..Chunk::SUBDIVIDED_CHUNK_SIZE {
-                for sd_z in 0..Chunk::SUBDIVIDED_CHUNK_SIZE {
-                    let ls_pos_sd = ivec3(sd_x, sd_y, sd_z);
+        for x in 0..Chunk::SIZE {
+            for y in 0..Chunk::SIZE {
+                for z in 0..Chunk::SIZE {
+                    let ls_pos = ivec3(x, y, z);
+                    let ws_pos = ls_pos + ws_min;
 
-                    if ls_pos_sd.cmplt(IVec3::splat(4)).all() {
-                        continue;
-                    }
+                    let avg_corner_noise = self.block_corner_noise(ws_pos);
 
-                    let ws_pos_sd = ws_min_sd + ls_pos_sd;
+                    if avg_corner_noise > THRESHOLD {
+                        sd_access.set(ls_pos, ChunkAccessInput::new(stone_block.clone()))?;
+                    // This test here is a decent-ish hueristic check that discards a lot of empty blocks (void) but
+                    // includes enough edges and corners and stuff that it makes the resulting terrain smoother
+                    } else if avg_corner_noise > (THRESHOLD / 2.0) {
+                        for mb_x in 0..SubdividedBlock::SUBDIVISIONS {
+                            for mb_y in 0..SubdividedBlock::SUBDIVISIONS {
+                                for mb_z in 0..SubdividedBlock::SUBDIVISIONS {
+                                    let mb_pos = ivec3(mb_x, mb_y, mb_z);
+                                    let ls_pos_sd =
+                                        mb_pos + (ls_pos * SubdividedBlock::SUBDIVISIONS);
 
-                    let noise_pos = ivec3(ws_pos_sd.x, ws_pos_sd.y, ws_pos_sd.z).as_dvec3()
-                        * (self.scale / 4.0);
-                    let noise = self.noise.get([noise_pos.x, noise_pos.y, noise_pos.z]);
+                                    let ws_pos_sd = ls_pos_sd + ws_min_sd;
 
-                    if noise > THRESHOLD {
-                        sd_access.set_mb(ls_pos_sd, Microblock::new(self.palette.stone))?;
+                                    let noise = self.noise_mb(ws_pos_sd);
+                                    if noise > THRESHOLD {
+                                        sd_access.set_mb(
+                                            ls_pos_sd,
+                                            Microblock::new(self.palette.stone),
+                                        )?;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
